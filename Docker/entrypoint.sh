b695c93217785852152888436987d0d4acb683d6
Code46-srz <123968213+Code46-srz@users.noreply.github.com>
2024-08-01 01:17:28 -0500

    v6

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -18,5 +18,5 @@ php artisan cache:clear
 php artisan config:clear
 php artisan route:clear
 
-php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
+php artisan serve --port=$PORT
 exec docker-php-entrypoint "$@"
